c11daa127860815acef6048be8066871393222a4
Aldo Serrano <aldoantonio1723@gmail.com>
2026-07-26 14:59:47 -0600

    fix: Cambia el tipo de dato en categoria - Tenia tipo de dato String y lo cambie a CategoriaEnergetica que es nuestro enum

--- backend/backend-api/src/main/java/com/hackathon/energia/dto/DatosRespuestaAnalisis.java
+++ backend/backend-api/src/main/java/com/hackathon/energia/dto/DatosRespuestaAnalisis.java
@@ -1,10 +1,12 @@
 package com.hackathon.energia.dto;
 
+import com.hackathon.energia.model.CategoriaEnergetica;
+
 import java.math.BigDecimal;
 import java.util.List;
 
 public record DatosRespuestaAnalisis(
-        String categoria,
+        CategoriaEnergetica categoria,
         Double probabilidad,
         List<String> recomendaciones,
         BigDecimal costoEstimadoMensual
