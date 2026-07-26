a33d1df4a12c2598930b70a8181c10bb0d653553
Aldo Serrano <aldoantonio1723@gmail.com>
2026-07-26 16:07:22 -0600

    feat: crea AnalisisController

---
+++ backend/backend-api/src/main/java/com/hackathon/energia/controller/AnalisisController.java
@@ -0,0 +1,28 @@
+package com.hackathon.energia.controller;
+
+import com.hackathon.energia.dto.DatosRegistroAnalisis;
+import com.hackathon.energia.dto.DatosRespuestaAnalisis;
+import com.hackathon.energia.service.AnalisisService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/analise-energetica")
+@RequiredArgsConstructor
+public class AnalisisController {
+
+    private final AnalisisService analisisService;
+
+    @PostMapping()
+    public ResponseEntity<DatosRespuestaAnalisis> analisar(@RequestBody @Valid DatosRegistroAnalisis datosAnalisis){
+       var resultado = analisisService.procesarAnalisis(datosAnalisis);
+       return ResponseEntity.status(HttpStatus.CREATED).body(resultado);
+    }
+
+}
