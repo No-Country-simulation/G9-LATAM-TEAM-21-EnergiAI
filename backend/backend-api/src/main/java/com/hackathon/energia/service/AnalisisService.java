391048b4c8978835a1e559a065356f44e1269c53
Aldo Serrano <aldoantonio1723@gmail.com>
2026-07-26 15:04:36 -0600

    feat: crea el método ProcesarAnalisis - Este método se encarga de llamar al modelo par realizar la predicción. - Crea nuestra entidad AnalisisEnergetico. - Lo guarda en la base de datos. - Retorna la predicción para ser mostrada al usuario final.

--- backend/backend-api/src/main/java/com/hackathon/energia/service/AnalisisService.java
+++ backend/backend-api/src/main/java/com/hackathon/energia/service/AnalisisService.java
@@ -1,7 +1,9 @@
 package com.hackathon.energia.service;
 
+import com.hackathon.energia.client.IaModelCliente;
 import com.hackathon.energia.dto.DatosRegistroAnalisis;
 import com.hackathon.energia.dto.DatosRespuestaAnalisis;
+import com.hackathon.energia.model.AnalisisEnergetico;
 import com.hackathon.energia.repository.AnalisisEnergeticoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,8 +13,24 @@ import org.springframework.stereotype.Service;
 public class AnalisisService {
 
     private final AnalisisEnergeticoRepository analisisEnergeticoRepository;
+    private final IaModelCliente iaModelCliente;
 
     public DatosRespuestaAnalisis procesarAnalisis(DatosRegistroAnalisis datos) {
+       DatosRespuestaAnalisis prediccion = iaModelCliente.obtenerPrediccion(datos);
 
+       AnalisisEnergetico analisis = AnalisisEnergetico.builder()
+               .consumoKwh(datos.consumoKwh())
+               .usoHorarioPico(datos.usoHorarioPico())
+               .cantidadEquipos(datos.cantidadEquipos())
+               .tipoInmueble(datos.tipoInmueble())
+               .horasAltoConsumo(datos.horasAltoConsumo())
+               .categoria(prediccion.categoria())
+               .probabilidad(prediccion.probabilidad())
+               .recomendaciones(prediccion.recomendaciones())
+               .costoEstimadoMensual(prediccion.costoEstimadoMensual())
+               .build();
+
+       analisisEnergeticoRepository.save(analisis);
+       return prediccion;
     }
 }
