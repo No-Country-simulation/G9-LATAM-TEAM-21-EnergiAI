259136ccd9ab7c31135c4b855467c7c086f74a70
Aldo Serrano <aldoantonio1723@gmail.com>
2026-08-05 17:09:10 -0600

    feat: crea la clase tokenService / - Para Generar el access token y verificarlo tambien

---
+++ backend/backend-api/src/main/java/com/hackathon/energia/service/TokenService.java
@@ -0,0 +1,50 @@
+package com.hackathon.energia.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.hackathon.energia.model.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    @Value("${api.security.token.issuer}")
+    private String issuer;
+
+    @Value("${api.security.token.access-expiration-minutes}")
+    private long accessMinutes;
+
+    public String generarAccessToken(Usuario usuario) {
+        try {
+            return JWT.create()
+                    .withIssuer(issuer)
+                    .withSubject(usuario.getLogin())
+                    .withClaim("uid", usuario.getId().toString())
+                    .withExpiresAt(Instant.now().plus(accessMinutes, ChronoUnit.MINUTES))
+                    .sign(Algorithm.HMAC256(secret));
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Error al generar token", e);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            return JWT.require(Algorithm.HMAC256(secret))
+                    .withIssuer(issuer)
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException e) {
+            throw new RuntimeException("Token inválido o expirado");
+        }
+    }
+}
