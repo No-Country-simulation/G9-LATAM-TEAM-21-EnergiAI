e12384688b255ba2c54c6a766c9ee938efd92bb8
Aldo Serrano <aldoantonio1723@gmail.com>
2026-08-05 15:55:12 -0600

    feat: implementar el registro de usuarios y gestionar la excepción de inicio de sesión existente

--- backend/backend-api/src/main/java/com/hackathon/energia/service/UsuarioService.java
+++ backend/backend-api/src/main/java/com/hackathon/energia/service/UsuarioService.java
@@ -1,11 +1,16 @@
 package com.hackathon.energia.service;
 
+import com.hackathon.energia.dto.DatosRegistroUsuario;
+import com.hackathon.energia.exception.ResourceAlreadyExistsException;
+import com.hackathon.energia.model.Usuario;
 import com.hackathon.energia.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,10 +18,22 @@ import org.springframework.stereotype.Service;
 public class UsuarioService implements UserDetailsService {
 
     private final UsuarioRepository repository;
+    private final PasswordEncoder encoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return repository.findByLogin(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
     }
+
+    @Transactional
+    public Usuario registro(DatosRegistroUsuario datos) {
+        if (repository.existsByLogin(datos.login())) {
+            throw new ResourceAlreadyExistsException("Login ya registrado");
+        }
+
+        var usuario = new Usuario(datos.login(), encoder.encode(datos.password()));
+        return repository.save(usuario);
+    }
+
 }
